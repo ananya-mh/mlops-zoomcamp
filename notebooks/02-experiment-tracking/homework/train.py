1bef4a3e9099435694cad32ea1edd14a5bfaabf3
ananya-mh <anu.mhegde@gmail.com>
2025-05-27 01:50:08 +0000

    hw 2

--- notebooks/02-experiment-tracking/homework/train.py
+++ notebooks/02-experiment-tracking/homework/train.py
@@ -1,9 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
+mlflow.set_tracking_uri('sqlite:///mlflow.db')
+mlflow.set_experiment('nyc-taxi-experiment')
 
 
 def load_pickle(filename: str):
@@ -19,14 +23,31 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    mlflow.sklearn.autolog()
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "ananya")
+        # mlflow.log_param("train_data_path", "./output/train.pkl")
+        # mlflow.log_param("val_data_path", "./output/val.pkl")
+        # mlflow.log_param("max_depth", 10)
+        # mlflow.log_param("random_state", 0)
+        # mlflow.log_param("model_type", "RandomForestRegressor")
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+
+        model_path = "../models/rf_model.bin"
+        with open(model_path, "wb") as f_out:
+            pickle.dump(rf, f_out)
+
+        mlflow.log_artifact(model_path, artifact_path="artifacts")
 
 
 if __name__ == '__main__':
